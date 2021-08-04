5743cd58c81e43e847ee0aac0736e465863146d9
Lycoris_nya <cat26022002@mail.ru>
2021-08-04 12:06:33 +0500

    test with bfs

--- tests/MakeLevelsTest.py
+++ tests/MakeLevelsTest.py
@@ -1,5 +1,31 @@
+import collections
 import unittest
 from main import Play_zone
+
+
+def bfs(graph, root):
+    queue = collections.deque([root])
+    graph[root[1]][root[0]] = 1
+    while queue:
+        neighbours = []
+        vertex = queue.popleft()
+        if vertex[0] - 1 >= 0 and graph[vertex[1]][vertex[0] - 1] != 1:
+            neighbours.append([vertex[0] - 1, vertex[1]])
+
+        if vertex[0] + 1 < Play_zone.NUMBER_BLOCKS_X and graph[vertex[1]][vertex[0] + 1] != 1:
+            neighbours.append([vertex[0] + 1, vertex[1]])
+
+        if vertex[1] - 1 >= 0 and graph[vertex[1] - 1][vertex[0]] != 1:
+            neighbours.append([vertex[0], vertex[1] - 1])
+
+        if vertex[1] + 1 < Play_zone.NUMBER_BLOCKS_Y and graph[vertex[1] + 1][vertex[0]] != 1:
+            neighbours.append([vertex[0], vertex[1] + 1])
+
+        for neighbour in neighbours:
+            graph[neighbour[1]][neighbour[0]] = 1
+            queue.append(neighbour)
+
+    return graph
 
 
 class MakeLevelsTest(unittest.TestCase):
@@ -8,6 +34,20 @@ class MakeLevelsTest(unittest.TestCase):
         for level in levels.keys():
             for coordinates in Play_zone.START_POSITION:
                 self.assertFalse(coordinates in levels[level])
+        self.assertTrue(True)
+
+    def test_all_place_available(self):
+        levels = Play_zone.make_levels()
+        for level in levels.keys():
+            play_zone = [[0] * Play_zone.NUMBER_BLOCKS_X for i in range(Play_zone.NUMBER_BLOCKS_Y)]
+            for coordinates in levels[level]:
+                play_zone[coordinates[1]][coordinates[0]] = 1
+            play_zone = bfs(play_zone, Play_zone.START_POSITION[0])
+            for x in range(Play_zone.NUMBER_BLOCKS_X):
+                for y in range(Play_zone.NUMBER_BLOCKS_Y):
+                    if play_zone[y][x] != 1:
+                        self.assertTrue(False)
+
         self.assertTrue(True)
 
 
